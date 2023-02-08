c67c490623efed81e38bd02e9f3dc958805a9eff
riyan-eng <fbriyant11@gmail.com>
2023-02-08 11:40:38 +0700

    create validator

--- util/validator.go
+++ util/validator.go
@@ -1,1 +1,26 @@
 package util
+
+import "github.com/go-playground/validator/v10"
+
+var validate = validator.New()
+
+type errorResponse struct {
+	FailedField string
+	Tag         string
+	Type        string
+}
+
+func Validate(input any) []*errorResponse {
+	var errors []*errorResponse
+	err := validate.Struct(input)
+	if err != nil {
+		for _, err := range err.(validator.ValidationErrors) {
+			var element errorResponse
+			element.FailedField = err.Field()
+			element.Tag = err.Tag()
+			element.Type = err.Kind().String()
+			errors = append(errors, &element)
+		}
+	}
+	return errors
+}
