247ee52c9fc38adbd516eab2eb2f25f0856583e6
riyan-eng <fbriyant11@gmail.com>
2023-02-08 10:22:19 +0700

    create dto

--- module/finance/controller/dto/journal_dto.go
+++ module/finance/controller/dto/journal_dto.go
@@ -1,1 +1,17 @@
 package dto
+
+type PurchaseJournal struct {
+	InventoryCode string  `json:"inventory_code" validate:"required"`
+	Quantity      uint    `json:"quantity" validate:"required"`
+	Price         float64 `json:"price" validate:"required"`
+	PPNIncome     bool    `json:"ppn_income" validate:"required"`
+	FreightPaid   float64 `json:"freight_paid"`
+}
+
+type SalesJournal struct {
+	InventoryCode    string  `json:"inventory_code" validate:"required"`
+	Quantity         uint    `json:"quantity" validate:"required"`
+	Price            float64 `json:"price" validate:"required"`
+	PPNOutcome       bool    `json:"ppn_outcome" validate:"required"`
+	FreightCollected float64 `json:"freight_collected"`
+}
